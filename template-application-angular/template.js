42a02997cc136ad2d3d852ba3c44e0b08ac19424
delchev <delchevn@gmail.com>
2018-03-21 15:43:52 +0200

    iterate collection

--- template-application-angular/template.js
+++ template-application-angular/template.js
@@ -7,21 +7,44 @@ exports.getTemplate = function() {
 		{
 			"location": "/template-application-angular/application.schema.template", 
 			"action": "generate",
-			"rename": "{{fileName}}.schema"
+			"rename": "{{fileName}}.schema",
+			"type": "db-schema"
 		},
 		{
 			"location": "/template-application-angular/application.js.template", 
 			"action": "generate",
 			"rename": "{{fileName}}.js",
-			"iterate": "entities"
+			"collection": "services",
+			"type": "rest-service"
 		},
 		{
 			"location": "/template-application-angular/index.html.template", 
 			"action": "generate",
 			"rename": "{{fileName}}.html",
 			"start" : "[[",
-			"end" : "]]"
-		}],
+			"end" : "]]",
+			"type": "ui-index"
+		},
+		{
+			"location": "/template-application-angular/manage.html.template", 
+			"action": "generate",
+			"rename": "{{fileName}}.html",
+			"start" : "[[",
+			"end" : "]]",
+			"collection": "collectionManage",
+			"type": "ui-manage"
+		},
+		{
+			"location": "/template-application-angular/list.html.template", 
+			"action": "generate",
+			"rename": "{{fileName}}.html",
+			"start" : "[[",
+			"end" : "]]",
+			"collection": "collectionList",
+			"type": "ui-list"
+		}
+
+		],
 		"parameters": []
 	};
 };
