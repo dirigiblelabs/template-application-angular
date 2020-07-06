9110403635fa5ff6f0e1383e70104c8e4df33315
ThuF <jordan.p.pavlov@gmail.com>
2020-07-06 14:27:25 +0300

    Formatting fixes

--- template-application-angular/template/template.js
+++ template-application-angular/template/template.js
@@ -8,69 +8,69 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-var apiTemplate = require('template-application-angular/template/api/template');
-var dataTemplate = require('template-application-angular/template/data/template');
-var uiTemplate = require('template-application-angular/template/ui/template');
-var feedTemplate = require('template-application-angular/template/feed/template');
+var apiTemplate = require("template-application-angular/template/api/template");
+var dataTemplate = require("template-application-angular/template/data/template");
+var uiTemplate = require("template-application-angular/template/ui/template");
+var feedTemplate = require("template-application-angular/template/feed/template");
 
-exports.getTemplate = function(parameters) {
+exports.getTemplate = function (parameters) {
 	return {
-		'name': 'Full-stack Application (AngularJS)',
-		'description': 'Full-stack Application with a Database Schema, a set of REST Services and an AngularJS User Interfaces',
-		'extension':'model',
-		'sources': getSources(parameters),
-		'parameters': [{
-			'name': 'extensionName',
-			'label': 'Extension',
-			'placeholder': 'Extension name'
+		name: "Full-stack Application (AngularJS)",
+		description: "Full-stack Application with a Database Schema, a set of REST Services and an AngularJS User Interfaces",
+		extension: "model",
+		sources: getSources(parameters),
+		parameters: [{
+			name: "extensionName",
+			label: "Extension",
+			placeholder: "Extension name"
 		}, {
-			'name': 'launchpadName',
-			'label': 'Launchpad',
-			'placeholder': 'Launchpad project name',
-			'ui': {
-				'hide': {
-					'property': 'includeLaunchpad',
-					'value': true
+			name: "launchpadName",
+			label: "Launchpad",
+			placeholder: "Launchpad project name",
+			ui: {
+				hide: {
+					property: "includeLaunchpad",
+					value: true
 				}
 			}
 		}, {
-			'name': 'includeLaunchpad',
-			'label': 'Embedded',
-			'type': 'checkbox'
+			name: "includeLaunchpad",
+			label: "Embedded",
+			type: "checkbox"
 		}, {
-			'name': 'title',
-			'label': 'Title',
-			'placeholder': 'Launchpad title',
-			'ui': {
-				'hide': {
-					'property': 'includeLaunchpad',
-					'value': false
+			name: "title",
+			label: "Title",
+			placeholder: "Launchpad title",
+			ui: {
+				hide: {
+					property: "includeLaunchpad",
+					value: false
 				}
 			}
 		}, {
-			'name': 'subTitle',
-			'label': 'Sub-title',
-			'placeholder': 'Launchpad sub-title',
-			'ui': {
-				'hide': {
-					'property': 'includeLaunchpad',
-					'value': false
+			name: "subTitle",
+			label: "Sub-title",
+			placeholder: "Launchpad sub-title",
+			ui: {
+				hide: {
+					property: "includeLaunchpad",
+					value: false
 				}
 			}
 		}, {
-			'name': 'description',
-			'label': 'Description',
-			'placeholder': 'Launchpad description',
-			'ui': {
-				'hide': {
-					'property': 'includeLaunchpad',
-					'value': false
+			name: "description",
+			label: "Description",
+			placeholder: "Launchpad description",
+			ui: {
+				hide: {
+					property: "includeLaunchpad",
+					value: false
 				}
 			}
 		}, {
-			'name': 'brand',
-			'label': 'Brand',
-			'placeholder': 'Brand'
+			name: "brand",
+			label: "Brand",
+			placeholder: "Brand"
 		}]
 	};
 };
